ea2eebab6815352fc11d7d5348f85412473a4818
Yerko Arce <yerko.arce.galaz@gmail.com>
2024-06-14 13:58:52 -0400

    change port name

--- index.js
+++ index.js
@@ -131,8 +131,8 @@ const unknownEndpoint = (request, response) => {
 // Se llama al middleware de las rutas inexistentes
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+const port = process.env.PORT || 3001
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
 })
  
